c4d31714f5df826216084e7348e185400c0c5646
PabloLopes19 <pabloelopes@hotmail.com>
2022-11-22 20:20:35 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const server = express();
+
+server.get(`/`, (req, res) => {
+    res.send("Raiz");
+});
+
+server.get("/api", (req, res) => {
+    if(req.query.som === "ping") res.send("Pong")
+    else if(req.query.som === "pong") res.send("Ping")
+    else res.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+});
+
+server.listen(3001, () => {
+    console.log("All systems ok");
+});
